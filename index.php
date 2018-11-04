0cd77356c3fe65fa8efb308ff7db213674b46bc3
kushibiki yuuki <41231171+yuuki-kushibiki@users.noreply.github.com>
2018-11-04 19:40:58 +0900

    Add files via upload

---
+++ index.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="ja" dir="ltr">
+<head>
+<meta charset="utf-8" />
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+<meta name="viewport" content="width=device-width,user-scalable=no,maximum-scale=1">
+<title>Mobile Peace</title>
+<link rel="shortcut icon" href="img/favicon.ico">
+</head>
+<body>
+<p>櫛引テスト</p>
+</body>
+</html>
