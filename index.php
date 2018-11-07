34d22467138b15814465379d44d9b2c65099c2e7
kushibiki yuuki <41231171+yuuki-kushibiki@users.noreply.github.com>
2018-11-07 19:25:51 +0900

    Add files via upload

--- index.php
+++ index.php
@@ -1,8 +1,100 @@
 <?php
-//COMPOSERでインストールしたライブラリー一括読み込み
-require_once __DIR__ . "/vendor/autoload.php";
+$accessToken = 'YYcTPvcAIdazJuNniN4vp6JzzsnGYudow342TumrrLvoTDdBPtVchEmnBj50iMZYMuXT1TMa5NNu52FSNeESdml0N8N7ReWkqrdcIXbSnKbNASc6YG186sPqkCWyX3pP2k9l4e5Z6dDErIz0pVa+oQdB04t89/1O/w1cDnyilFU=';
+$jsonString = file_get_contents('php://input'); error_log($jsonString);
+$jsonObj = json_decode($jsonString); $message = $jsonObj->{"events"}[0]->{"message"};
+$replyToken = $jsonObj->{"events"}[0]->{"replyToken"};
 
-$inputString = file_get_contents('php://input');
-error_log($inputString);
 
- ?>
+ // 送られてきたメッセージの中身からレスポンスのタイプを選択
+if ($message->{"text"} == '確認') {
+     // 確認ダイアログタイプ
+    $messageData = [
+        'type' => 'template',
+        'altText' => '確認ダイアログ',
+        'template' => [ 'type' => 'confirm', 'text' => '元気ですかー？',
+            'actions' => [
+                [ 'type' => 'message', 'label' => '元気です', 'text' => '元気です' ],
+                [ 'type' => 'message', 'label' => 'まあまあです', 'text' => 'まあまあです' ], 
+            ]
+        ]
+ ];
+} elseif ($message->{"text"} == 'ボタン') {
+    // ボタンタイプ
+    $messageData = [
+        'type' => 'template',
+         'altText' => 'ボタン',
+        'template' => [
+             'type' => 'buttons',
+             'title' => 'タイトルです',
+             'text' => '選択してね',
+            'actions' => [
+                 [
+                    'type' => 'postback',
+                    'label' => 'webhookにpost送信',
+                    'data' => 'value'
+                ],
+                 [
+                     'type' => 'uri',
+                     'label' => 'googleへ移動',
+                     'uri' => 'https://google.com'
+                 ]
+              ]
+          ]
+     ];
+} elseif ($message->{"text"} == 'カルーセル') {
+     // カルーセルタイプ
+    $messageData = [
+        'type' => 'template',
+        'altText' => 'カルーセル',
+        'template' => [
+             'type' => 'carousel',
+            'columns' => [
+                [
+                    'title' => 'カルーセル1',
+                    'text' => 'カルーセル1です',
+                     'actions' => [
+                         [
+                            'type' => 'postback',
+                             'label' => 'webhookにpost送信',
+                             'data' => 'value'
+                         ],
+                         [
+                            'type' => 'uri',
+                            'label' => '美容の口コミ広場を見る',
+                             'uri' => 'http://clinic.e-kuchikomi.info/'
+                         ]
+                    ]
+                ],
+                 [
+                        'title' => 'カルーセル2',
+                        'text' => 'カルーセル2です',
+                        'actions' => [
+                            [
+                                'type' => 'postback',
+                                'label' => 'webhookにpost送信',
+                                'data' => 'value'
+                            ],
+                            [
+                                'type' => 'uri',
+                                'label' => '女美会を見る',
+                                'uri' => 'https://jobikai.com/'
+                            ]
+                        ]
+                    ],
+                ]
+            ]
+    ];
+ } else {
+     // それ以外は送られてきたテキストをオウム返し
+     $messageData = [ 'type' => 'text', 'text' => $message->{"text"} ];
+}
+$response = [ 'replyToken' => $replyToken, 'messages' => [$messageData] ];
+error_log(json_encode($response));
+$ch = curl_init('https://api.line.me/v2/bot/message/reply');
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($response));
+curl_setopt($ch, CURLOPT_HTTPHEADER, array( 'Content-Type: application/json; charser=UTF-8', 'Authorization: Bearer ' . $accessToken ));
+$result = curl_exec($ch); error_log($result);
+curl_close($ch);
