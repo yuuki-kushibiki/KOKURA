dae663943d139d2ef825b8d74556d4968cb13ee2
kushibiki yuuki <41231171+yuuki-kushibiki@users.noreply.github.com>
2018-11-07 20:22:34 +0900

    Add files via upload

--- index.php
+++ index.php
@@ -105,41 +105,6 @@ if ($message->{"text"} == '出勤確認') {
      // それ以外は送られてきたテキストをオウム返し
      $messageData = [ 'type' => 'text', 'text' => $message->{"text"} ];
 }
-{
-  "type": "text", // ①
-  "text": "Select your favorite food category or send me your location!",
-  "quickReply": { // ②
-    "items": [
-      {
-        "type": "action", // ③
-        "imageUrl": "https://example.com/sushi.png",
-        "action": {
-          "type": "message",
-          "label": "Sushi",
-          "text": "Sushi"
-        }
-      },
-      {
-        "type": "action",
-        "imageUrl": "https://example.com/tempura.png",
-        "action": {
-          "type": "message",
-          "label": "Tempura",
-          "text": "Tempura"
-        }
-      },
-      {
-        "type": "action", // ④
-        "action": {
-          "type": "location",
-          "label": "Send location"
-        }
-      }
-    ]
-  }
-}
-
-
 
 $response = [ 'replyToken' => $replyToken, 'messages' => [$messageData] ];
 error_log(json_encode($response));
