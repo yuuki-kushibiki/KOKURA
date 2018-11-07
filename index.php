c3ee98acc43794e712e058f54a6e0d16c12e1c49
kushibiki yuuki <41231171+yuuki-kushibiki@users.noreply.github.com>
2018-11-07 19:02:00 +0900

    Add files via upload

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 //COMPOSERでインストールしたライブラリー一括読み込み
-require_once __DIR__ . ’/vendor/autoload.php’;
+require_once __DIR__ . "/vendor/autoload.php";
 
 $inputString = file_get_contents('php://input');
 error_log($inputString);
