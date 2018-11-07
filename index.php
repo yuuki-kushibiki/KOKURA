d971f8f9027c34ce871ed10e7f8aa4306f3994f3
kushibiki yuuki <41231171+yuuki-kushibiki@users.noreply.github.com>
2018-11-07 17:47:03 +0900

    Add files via upload

--- index.php
+++ index.php
@@ -2,13 +2,13 @@
 
 require_once __DIR__ . ’/vendor/autoload.php’;
 
-$httpClient = new LINE\LINEBot\HTTPClient\CurlHTTPClient(getenv
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(getenv
     ('CHANNEL_ACCESS_TOKEN'));
 
-$bot =new \LINE\LINEBot($httpClient,['channelSecret' => getenv(
+$bot = new \LINE\LINEBot($httpClient,['channelSecret' => getenv(
     'CHANNEL_SECRET')]);
 
-$signature = $_SERVER['HTTP_' . LINE\LINEBot\Constant\HTTPHeader
+$signature = $_SERVER['HTTP_' . \LINE\LINEBot\Constant\HTTPHeader
     ::LINE_SIGNATURE];
 
 $events = $bot->parseEventRequest(file_get_contents('php://input'),
