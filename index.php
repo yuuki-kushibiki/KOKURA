cd665ba26b65d85ce04aa1e72956f593c1a08f32
kushibiki yuuki <41231171+yuuki-kushibiki@users.noreply.github.com>
2018-11-07 18:53:05 +0900

    Add files via upload

--- index.php
+++ index.php
@@ -2,15 +2,7 @@
 //COMPOSERでインストールしたライブラリー一括読み込み
 require_once __DIR__ . ’/vendor/autoload.php’;
 
-$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(getenv('CHANNEL_ACCESS_TOKEN'));
+$inputString = file_get_contents('php://input');
+error_log($inputString);
 
-$bot = new \LINE\LINEBot($httpClient,['channelSecret' => getenv('CHANNEL_SECRET')]);
-
-$signature = $_SERVER['HTTP_' . \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
-
-$events = $bot->parseEventRequest(file_get_contents('php://input'),$signature);
-
-foreach ($events as $event) {
-  $bot->replyText($event->getReplyToken(), 'TextMessage');
-}
  ?>
