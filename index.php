e6e4721ee29ef87ca25f7444a3bc58f8c4693b64
kushibiki yuuki <41231171+yuuki-kushibiki@users.noreply.github.com>
2018-11-14 01:57:41 +0900

    Add files via upload

--- index.php
+++ index.php
@@ -1,19 +1,20 @@
 <?php
 require_once("./vendor/autoload.php");
 
-$httpClient= new \ LINE \ LINEBot \ HTTPClient \ CurlHTTPClient('YYcTPvcAIdazJuNniN4vp6JzzsnGYudow342TumrrLvoTDdBPtVchEmnBj50iMZYMuXT1TMa5NNu52FSNeESdml0N8N7ReWkqrdcIXbSnKbNASc6YG186sPqkCWyX3pP2k9l4e5Z6dDErIz0pVa+oQdB04t89/1O/w1cDnyilFU=');
+$httpClient= new \ LINE \ LINEBot \ HTTPClient \ CurlHTTPClient('4HloHdjQY7BBCGIzdqUGQW/x6bjbfDq2ceHm+JN3HWpRnum1z3QYYaXrju7qKLin1NwJ4mcLAE2JLvC5v9D5idsj576xaqLc46Tg7yjUo2C50iN6rKmUExaK22zc7HqTXfg9RG5MQ7vhCA2esNLn7AdB04t89/1O/w1cDnyilFU=');
 
-$bot= new \ LINE \ LINEBot($httpClient,['channelSecret' => '8dcb7ea00ac740c15259b6e9748229dd']);
+$bot= new \ LINE \ LINEBot($httpClient,['channelSecret' => '331be4568cb5f875bb56f7d5ffb0d17b']);
 
 $signature = $_SERVER['HTTP_' . \ LINE \ LINEBot \ Constant \ HTTPHeader::LINE_SIGNATURE];
 
 $events = $bot->parseEventRequest(file_get_contents('php://input'),$signature);
 
 foreach ($events as $event) {
-//replyTextMessage($bot, $event->getReplyToken(), 'TextMessage');
-replyLocationMessage($bot, $event->getReplyToken(), 'LINE' ,
-'東京都渋谷区渋谷ヒカリエ',
-35.659025,139.703473);
+//複数まとめて動作
+replyMultiMessage($bot, $event->getReplyToken(),new \ LINE \ LINEBot \ MessageBuilder \ TextMessageBuilder('TextMessage'),
+new \ LINE \ LINEBot \ MessageBuilder \ ImageMessageBuilder('https://' . $_SERVER['HTTP_HOST'] .'/imgs/original.jpg', 'https://' .
+$_SERVER['HTTP_HOST'] . '/imgs/preview.jpg'),new \ LINE \ LINEBot \ MessageBuilder \ LocationMessageBuilder('kushibiki',
+'東京都渋谷区渋谷kushibiki',35.659025,139.703473),new \ LINE \ LINEBot \ MessageBuilder \ StickerMessageBuilder(1, 1));
 }
 
 function replyTextMessage($bot, $replyToken, $text){
@@ -37,4 +38,36 @@ if(!$response->isSucceeded()) {
 error_log('Failed! '. $response->getHTTPStatus .' '. $response->getRAwBody());
  }
 }
+//複数まとめて動作
+function replyMultiMessage($bot, $replyToken, ...$imgs){
+$builder = new \ LINE \ LINEBot \ MessageBuilder \ MultiMessageBuilder();
+
+foreach ($msgs as $value) {
+  $builder->add($value);
+}
+$response = $bot->replyMessage($replyToken, $builder);
+if(!$response->isSucceeded()){
+error_log('Failed! '. $response->getHTTPStatus .' '. $response->getRAwBody());
+}
+}
+
+//buttonsテンプレート
+//function replyButtonsTemplate($bot, $replyToken, $alternativeText, $imageUrl, $title, $text, ...$actions){
+//$actionArray = array();
+
+//foreach ($actions as $value) {
+//  array_push($actionArray, $value);
+//}
+  //$builder = new \ LINE \ LINEBot \ MessageBuilder \ TemplateMessageBuilder($alternativeText,
+//new \ LINE \ LINEBot \ MessageBuilder \ TemplateMessageBuilder\ ButtonTemplateBuilder($title, $text, $imageUrl, $actionArray));
+
+//$response = $bot->replyMessage($replyToken, $builder);
+//if(!$response->isSucceeded()){
+//error_log('Failed! '. $response->getHTTPStatus .' '. $response->getRAwBody());
+//}
+
+//}
+
+
+
 ?>
